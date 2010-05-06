2754a0dfa8f68c82c15f2a5ec32366f8b297e1d8
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2010-05-06 10:23:53 +0000

    Zoom to extent of features on 'loadend'. Register initMap with the Plone onload registry.

--- pleiades/openlayers/browser/ol-map.js
+++ pleiades/openlayers/browser/ol-map.js
@@ -36,39 +36,49 @@ function onFeatureUnselect(feature) {
 }
 
 function initMap() {
-    var jsonURI = getJSON();
-    map = new OpenLayers.Map(
-                'map', {
-                    projection: new OpenLayers.Projection("EPSG:900913"),
-                    displayProjection: new OpenLayers.Projection("EPSG:4326"),
-                    units: "m",
-                    numZoomLevels: 18,
-                    maxResolution: 156543.0339,
-                    maxExtent: new OpenLayers.Bounds(-20037508, -20037508, 20037508, 20037508)
-                    }
-                );
-    base = new OpenLayers.Layer.Google(
-                "Base", {   
-                    isBaseLayer: true,
-                    type: G_PHYSICAL_MAP, 
-                    sphericalMercator: true
-                    }
-                );
-        
-    vectors = new OpenLayers.Layer.GML(
-                "Place",
-                jsonURI,
-                {format: OpenLayers.Format.GeoJSON}
-                );
 
-    map.addLayers([base, vectors]);
-    map.addControl(new OpenLayers.Control.MousePosition());
-    
-    selectControl = new OpenLayers.Control.SelectFeature(
-                        vectors,
-                        {onSelect: onFeatureSelect, onUnselect: onFeatureUnselect}
-                        );    
-    map.addControl(selectControl);
-    selectControl.activate();
-    map.zoomToMaxExtent();
+  map = new OpenLayers.Map('map', {
+    projection: new OpenLayers.Projection("EPSG:900913"),
+    displayProjection: new OpenLayers.Projection("EPSG:4326"),
+    units: "m",
+    numZoomLevels: 18,
+    maxResolution: 156543.0339,
+    maxExtent: new OpenLayers.Bounds(-20037508, -20037508, 20037508, 20037508)
+    });
+
+  base = new OpenLayers.Layer.Google('Base', {   
+    isBaseLayer: true,
+    type: G_PHYSICAL_MAP, 
+    sphericalMercator: true
+    });
+
+  var jsonURI = getJSON();
+  
+  vectors = new OpenLayers.Layer.Vector("Place", {
+    strategies: [new OpenLayers.Strategy.Fixed()],
+    protocol: new OpenLayers.Protocol.HTTP({
+      url: jsonURI,
+      format: new OpenLayers.Format.GeoJSON()})
+    });
+
+  vectors.events.on({'loadend' : function(e) {
+    var extent = vectors.getDataExtent();
+    extent.left -= 5000.0;
+    extent.right += 5000.0;
+    extent.top += 5000.0;
+    extent.bottom -= 5000.0;
+    map.zoomToExtent(extent);
+    }});
+
+  map.addLayers([base, vectors]);
+  map.addControl(new OpenLayers.Control.MousePosition());
+  selectControl = new OpenLayers.Control.SelectFeature(
+                      vectors,
+                      {onSelect: onFeatureSelect, 
+                       onUnselect: onFeatureUnselect});    
+  map.addControl(selectControl);
+  selectControl.activate();
+  map.zoomToMaxExtent();
 }
+
+registerPloneFunction(initMap);
